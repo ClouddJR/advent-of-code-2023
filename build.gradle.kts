1f12dc27bc328b11e776a2380a54b550657f5d77
Arkadiusz Chmura <arekchmura@gmail.com>
2023-11-30 20:20:18 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
